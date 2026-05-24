7178662ee9cd23713346db8d385c48b8b8996c3a
Mikel <mikel_cortina@euneiz.com>
2026-05-24 15:31:53 +0200

    Update Movement System

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -3,17 +3,23 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    [Header("Stats")]
+    [Header("Movement")]
     public float moveSpeed = 5f;
+    public float acceleration = 18f;
+    public float deceleration = 22f;
 
     private Rigidbody2D _rb;
-    private Vector2 _moveInput;
+    private Vector2 _input;
+    private Vector2 _currentVelocity;
 
-    void Awake() => _rb = GetComponent<Rigidbody2D>();
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        _moveInput = new Vector2(
+        _input = new Vector2(
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
         ).normalized;
@@ -21,6 +27,16 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        _rb.MovePosition(_rb.position + _moveInput * moveSpeed * Time.fixedDeltaTime);
+        Vector2 targetVelocity = _input * moveSpeed;
+
+        float accelRate = (_input.sqrMagnitude > 0.01f) ? acceleration : deceleration;
+
+        _currentVelocity = Vector2.MoveTowards(
+            _currentVelocity,
+            targetVelocity,
+            accelRate * Time.fixedDeltaTime
+        );
+
+        _rb.linearVelocity = _currentVelocity;
     }
 }
